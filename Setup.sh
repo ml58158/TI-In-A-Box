48b7862390d7a5a572ba114faa96ea231eb8ed3d
Matt Larkin <mattlarkin@me.com>
2022-09-26 12:55:33 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,22 +3,19 @@
 #1. Update/Upgrade System, if needed
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-#2. Create a Local Password for MISP User
-sudo passwd misp
-
-#3. Install XRDP
+#2. Install XRDP
 sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
 sudo ufw allow 3389
 
-#4. Install Xfce4 Desktop
+#3. Install Xfce4 Desktop
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
 
-#5. Install Firefox Browser
+#4. Install Firefox Browser
 sudo apt install firefox -y
 
-#6. Install MISP with install.sh
+#5. Install MISP with install.sh
 # This will install MISP Core
 wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
 bash /tmp/INSTALL.sh -c
